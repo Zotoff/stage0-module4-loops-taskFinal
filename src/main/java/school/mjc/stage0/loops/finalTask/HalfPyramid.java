9faf7cf822da32a40be3b995260d4ca25213dc8e
Zotoff <paulzotoff@gmail.com>
2023-06-21 22:22:58 +0300

    First attempt

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,19 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
+        for (int y = 1; y <= cathetusLength; y++){
+            System.out.print(" ".repeat(cathetusLength-y));
+            for(int x = 1; x <= cathetusLength; x++){
+                if (y >= x){
+                    System.out.print("*");
+                }
+            }
+            System.out.println("");
 
+        }
+        if (cathetusLength == 0){
+            System.out.print("");
+        }
     }
 
     public static void main(String[] args) {
